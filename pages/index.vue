cf3b7f5a98a54d6bb6da1fd1f72709bc745253f0
sergey.kalinin <sergey.kalinin@sebbia.org>
2024-04-25 19:08:15 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <span class="title"> Привет это гланая </span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.wrapper {
+  border: 1px solid black;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .title {
+    text-align: center;
+    color: gold;
+  }
+}
+
+</style>
