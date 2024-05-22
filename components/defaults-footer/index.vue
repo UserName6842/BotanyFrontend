aff587d713b96f1f8dda5946ff30232c1df8f3b3
Денис Максименко <sergey.kalinin@sebbia.org>
2024-05-23 00:07:33 +0300

    v1.0.0 add footer

---
+++ components/defaults-footer/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="w-[220px] text-center">
+    10:00-20:00 MSK • GMT +3
+    hello@result.school
+  </div>
+  <div class="flex  flex-col items-center justify-center mr-24">
+      <NuxtLink href="/home" target="_blank">
+        Документация
+      </NuxtLink>
+    <a href="https://donnu.ru/bio/bec" target="_blank">
+        О нас
+      </a>
+  </div>
+  <div class="icon-wrapper">
+    <a href="https://t.me/Kalin24Don" target="_blank">
+      <telegram/>
+    </a>
+    <a href="https://vk.com/bio_profkom" target="_blank">
+      <vkontacte/>
+    </a>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import telegram from "assets/icons/footer/telegram.svg"
+import vkontacte from "assets/icons/footer/vkontacte.svg"
+</script>
+
+<style lang="scss" scoped>
+.icon-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 122px;
+  height: 32px;
+
+  svg {
+    width: 32px;
+    height: 32px;
+  }
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,28 @@
 <template>
-<div class="flex flex-col items-center justify-between w-full h-screen ">
-  <UContainer class="h-20 w-full pt-2">
-    <defaults-header/>
-  </UContainer>
-  <UContainer class="flex-1">
-    <slot name="default"/>
-  </UContainer>
-  <UContainer class="h-20 w-full">
-    footer
-  </UContainer>
-</div>
+  <div class="flex flex-col items-center justify-between w-full h-screen ">
+    <UContainer class="h-20 w-full pt-2">
+      <defaults-header/>
+    </UContainer>
+    <UContainer class="flex-1">
+      <slot name="default"/>
+    </UContainer>
+    <UContainer class="footer h-20 w-full">
+      <defaults-footer/>
+    </UContainer>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
+.footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  gap: 30px;
+  border-top: 1px solid var(--ling-root);
 
+}
 </style>
