2d0040cbdd3b54c1c2306b1cf3bc1d9b579315db
Денис Максименко <sergey.kalinin@sebbia.org>
2024-06-16 17:23:36 +0300

    v1.0.0 fix Dockerfile

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -31,13 +31,12 @@ const login = async (value: ModelAuth) => {
     const { mutate, onDone, onError } = useMutation(mutation);
 
     onDone(async (data) => {
+      console.log("Авторизация:",data.data)
       const refreshToken = useCookie("refresh_token");
       refreshToken.value = data.data.auth.signInUser.refresh_token;
       const { onLogin } = useApollo();
       await onLogin(data.data.auth.signInUser.access_token);
       console.log("Авторизация:",data.data.auth.signInUser.access_token)
-      const auth = useAuth();
-      auth.setIsLogin(true);
       navigateTo("/home");
     });
 
